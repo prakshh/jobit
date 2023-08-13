6e911090ccc6f6c3cf60bb2f164ff7f800e6be55
prakshh <prakashdas18@gmail.com>
2023-08-13 19:20:37 +0530

    searchContainer

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -24,8 +24,20 @@ const Welcome = () => {
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
           <TextInput 
-            style={styles.searchInput}/>
+            style={styles.searchInput}
+            value=""
+            onchange={() => {}}
+            placeholder='What are you looking for?'
+          />
         </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode='contain'
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
       </View>
     </View>
   )
